87f9c756433723e9ea8bbdcf84a3d086bde15a1f
jdx <librededrogas420@gmail.com>
2023-06-03 12:16:05 -0500

    almost i finish the store

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,8 +1,30 @@
 let navigation = document.querySelector('.navigation');
 let closebackground = document.querySelector('.nav-close-out');
-
+let cartSlideBar = document.querySelector('.nav-slide-cart');
 function showSlideBar(){
-
-  navigation.classList.toggle('show');
+  
+  if(cartSlideBar.classList.contains('show')){
+    
+  cartSlideBar.classList.toggle('show');
+  }else{
   closebackground.classList.toggle('show');
+  }
+  navigation.classList.toggle('show');
+}
+
+function showCart(){
+  if(navigation.classList.contains('show')){
+
+    navigation.classList.toggle('show');
+  }else{
+
+  closebackground.classList.toggle('show');
+  }
+  cartSlideBar.classList.toggle('show');
+}
+
+function hiddenSlideBars(){
+  navigation.classList.remove('show');
+  cartSlideBar.classList.remove('show');
+  closebackground.classList.remove('show');
 }
